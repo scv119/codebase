7146f49080be2f78aba04db672b5bc53bc0f0c21
shen chen <scv119@gmail.com>
2012-01-18 14:00:20 +0800

    123

--- algorithm/select.c
+++ algorithm/select.c
@@ -3,71 +3,67 @@ void __sort(int * array, int from , int end);
 void __debug(int * array, int from, int end);
 void swap(int * array, int from, int end);
 
-int select(int * array, int length, int k, int from, int end)
+int select(int * array, int k, int size)
 {
-    printf("start k=%d,from=%d,end=%d\n",k,from,end);
     int i;
-    if ( k >= length || length <= 0 || from < 0 || end > length)
+    if ( k >= size || k < 0)
         return -1;
     
 
-    if (end - from <= 5)
+    if (size <= 5)
     {
-        __sort(array, from, end);
-        __debug(array, from, end);
-        printf("mid=%d\n", array[k]);
-        printf("k=%d, pos=%d\n", k, k);
+        __sort(array, 0, size);
         return array[k];
     }
             
-    int val = (end - from )/5;
+    int val = size /5;
     
     for ( i = 0 ; i < val ; i ++ ){
-        __sort(array, from + i * 5 , from + i * 5 + 5);
+        __sort(array, i * 5 , i * 5 + 5);
     }
 
     for ( i = 0 ; i < val ; i ++ ){
-        swap (array, from + i, from + i * 5 + 2);
+        swap (array,  i,  i * 5 + 2);
     }
 
 
-    int mid = select(array, length,  from + val/2 ,from , val + from);
+    int mid = select(array, val/2 ,val);
     
-    __debug(array, from, end);
     printf("mid=%d\n", mid);
 
-    int start = from;
-    int equal = from;
-    int endp   = end-1;
+    int small_p = 0;
+    int equal_p = 0;
+    int big_p = size-1;
 
-    while(start <= endp){
-        if (array[start] > mid){
-            swap(array, start, endp);
-            endp --;
+    while ( small_p <=  big_p ){
+        value = array[small_p];
+        if (value < mid){
+            small_p ++;
         }
-        else if (array[start] < mid){
-            start ++;
+        else if(value > mid){
+            swap(array, small_p, big_p);
+            big_p --;
         }
         else {
-            swap(array, equal, start); 
-            equal ++;
-            start ++;
+            swap(array, equal_p,small_p); 
+            equal_p ++;
+            small_p ++;
         }
     }
+    
+    int pos = big_p;
 
-    int pos = start - 1;
-
-    for ( i = from; i < equal; i ++ ){
-        swap(array, i, --start);
+    for (i = 0 ; i < equal_p; i ++){
+       swap(array, i, big_p);
+       big_p --;
     }
 
-    __debug(array, from, end);
     printf("k=%d, pos=%d\n", k, pos);
  
     if(k > pos)
-        return select(array, length, k ,  pos + 1, end);
+        return select(array + pos + 1, k - pos - 1 ,length - pos - 1 );
     else if(k < pos)
-        return select(array, length, k, from,  pos - 1);
+        return select(array, k,  pos);
     else return mid;
     
 }
