d23a0044a2a75bc00482e3016c11c0981d0b56b6
Shen Chen <scv119@gmail.com>
2012-03-07 00:44:37 +0800

    drop ball dp and mr

--- algorithm/dp/DropBall.java
+++ algorithm/dp/DropBall.java
@@ -1,7 +1,6 @@
 import java.util.*;
 public class DropBall{
-    private static Map<String, Integer> map = new HashMap<String,Integer>();
-    public static int drop_ball(int n, int k){
+    public static int drop_ball(int n, int k, int[][] map){
         if (k == 1){
             return n;
         }
@@ -14,59 +13,60 @@ public class DropBall{
             return 1;
         }
 
-        String key = n + " "+ k;
-        if(map.containsKey(key))
-            return map.get(key);
+        if( map == null){
+            map = new int[n+1][k+1];
+            for(int i = 0; i < n + 1 ; i ++){
+                for(int j = 0 ; j < k + 1 ;j ++){
+                    map[i][j] = -1;
+                }
+            }
+        }
+
         int min = Integer.MAX_VALUE;
         for(int i = 1 ; i <= n ; i ++){
-            int value = Math.max(drop_ball(i - 1, k -1), drop_ball(n - i, k)) + 1; 
+            int first = map[i-1][k-1];
+            if(first == -1){
+                first = drop_ball(i - 1, k - 1, map);
+            }
+            int second = map[n-i][k];
+            if(second == -1){
+               second = drop_ball(n-i, k, map);
+            }
+            int value = Math.max(first, second) + 1; 
             if (value < min){
                 min = value;
             }
         }
 
-        map.put(key,min);
+        map[n][k] = min;
         return min;
     }
 
     public static int drop_ball_dp(int n , int k){
         int [][] array = new int[n+1][k+1];
-        for(int j = 0 ; j <= k ; j++){
-            for (int i = 0 ; i <= n; i ++){
-                if ( i == 1){
-                    array[i][j] = j;
+        for(int i = 1 ; i <= n ; i ++){
+            array[i][1] = i;
+        }
+        for (int j = 2; j<=k ;j ++){
+            for(int i = 1; i<= n ; i ++){
+                int min = Integer.MAX_VALUE;
+                for ( int r = 1; r <= i; r++){
+                    int value = Math.max(array[r-1][j-1], array[i-r][j]) + 1;
+                    if(value < min)
+                        min = value;
                 }
-
-                else if ( j == 0){
-                    array[i][j] = 0;
-                }
-
-                else if (j == 1){
-                    array[i][j] = 1;
-                }
-
-                else{
-                    
-                    int min = Integer.MAX_VALUE;
-                    for( int l = 1 ; l <= i ; l++){
-                        int value = Math.max(array[l -1][ j -1] , array[i - l][ j]) + 1;
-                        if(value < min){
-                            min = value;
-                        }
-                    }
-                    array[i][j] = min;
-                }
-
+                array[i][j] = min;
             }
         }
-
         return array[n][k];
+        
 
     }
 
     public static void main(String args[]){
-        System.out.println(drop_ball(300,3));
+        System.out.println(drop_ball(300,3, null));
         System.out.println(drop_ball_dp(300,3));
-        System.out.println(map.size());
+        System.out.println(drop_ball(10000,3, null));
+        System.out.println(drop_ball_dp(10000,3));
     }
 }
