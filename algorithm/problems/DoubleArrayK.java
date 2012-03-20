c9f939d483495004a38f6295a8c8adc8c8541b90
Shen Chen <scv119@gmail.com>
2012-03-21 04:13:20 +0800

    to do

---
+++ algorithm/problems/DoubleArrayK.java
@@ -0,0 +1,15 @@
+public class DoubleArrayK{
+    public int findK(int k, int arr1[], int arr2[])
+    {
+        return findK(k, 0, arr1.length, 0, arr2.length, arr1, arr2);
+    }
+
+    private int findK(int k, int s1, int l1, int s2, int l2, int arr1[], int arr2[]){
+         if(l1 == 0){
+            
+         }
+         if(l2 == 0){
+            
+         }
+    }
+}
