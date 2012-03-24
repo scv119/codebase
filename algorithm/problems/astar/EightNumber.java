2b47a9f5a4cb179fe229b746c3c364aa2db3360d
Shen Chen <scv119@gmail.com>
2012-03-24 15:22:14 +0800

    finish a star for eight numbers

--- algorithm/problems/astar/EightNumber.java
+++ algorithm/problems/astar/EightNumber.java
@@ -1,10 +1,11 @@
+import java.util.*;
 public class EightNumber{
     public class Node{
         int state;
         int depth;
         int g;
         Node pre;
-        public Node(state){
+        public Node(int state){
             this.state = state;
         }
         
@@ -13,19 +14,57 @@ public class EightNumber{
         }
         String sState(){
             String s= state + "";
-            if(s.length == 8)
+            if(s.length() == 8)
                 s = "0"+s;
             return s;
         }
-        int[] otherState(){
-            
+        List<Integer> otherState(){
+            List<Integer> result = new LinkedList<Integer>();
+            String sstate = sState();
+            int pos = 0;
+            for(;pos < sstate.length(); pos ++){
+                if(sstate.charAt(pos) == '0')
+                    break;
+            }
+            int x = pos / 3;
+            int y = pos % 3;
+            int new_x;
+            int new_y;
+            char[] new_array = new char[9];
+            for(int j = -1; j <=1 ;j +=2){
+                new_x = j + x;
+                new_y = y;
+                if(new_x >= 0 && new_x <= 2){
+                    for(int k = 0 ; k < 9; k ++){
+                        new_array[k] = sstate.charAt(k);
+                     }
+                    int new_pos = new_x * 3 + new_y; 
+                    char tmp = new_array[pos];
+                    new_array[pos] = new_array[new_pos];
+                    new_array[new_pos] = tmp;
+                    result.add(Integer.parseInt(new String(new_array)));
+                }
+                new_x = x;
+                new_y = j + y;
+                if(new_y >= 0 && new_y <= 2){
+                    for(int k = 0 ; k < 9; k ++){
+                        new_array[k] = sstate.charAt(k);
+                     }
+                    int new_pos = new_x * 3 + new_y; 
+                    char tmp = new_array[pos];
+                    new_array[pos] = new_array[new_pos];
+                    new_array[new_pos] = tmp;
+                    result.add(Integer.parseInt(new String(new_array)));
+                }
+            }
+            return result;
         }
     }
 
     int get_g ( Node cur, Node des){
         int diff = 0;
-        String cstate = cur.sState ;
-        String dstate = des.sState ;
+        String cstate = cur.sState() ;
+        String dstate = des.sState() ;
         for(int i = 0 ; i < 9 ; i ++){
            if(cstate.charAt(i) != dstate.charAt(i))
             diff ++ ;
@@ -49,9 +88,38 @@ public class EightNumber{
 
         queued.add(init.state);
         q.add(init);
+        Node find = null;
         while(q.size() > 0){
             Node node = q.poll();
-            for(
+            for(Integer state:node.otherState()){
+                if(queued.contains(state))
+                    continue;
+                Node child = new Node(state);
+                child.depth = node.depth + 1;
+                child.g = get_g(child, des);
+                child.pre = node;
+                if(child.g == 0)
+                {
+                    find = child;
+                    break;
+                }
+                q.add(child);
+            }
+            if(find != null)
+                break;
         }
+        if(find == null)
+            System.out.println("No answer");
+        else{
+            while(find != null)
+            {
+                System.out.println(find.sState());
+                find = find.pre;
+            }
+        }
+    }
+    public static void main(String args[]){
+        EightNumber en = new EightNumber();
+        en.solve(102453786, 123456780);
     }
 }
