53aab86ff0cec22e4fba51873fbec23f07f7b6c3
shen chen <scv119@gmail.com>
2012-03-26 22:07:30 +0800

    buggy network flow

--- algorithm/network-flow/NetworkFlow.java
+++ algorithm/network-flow/NetworkFlow.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class NetworkFlow{
     int [][] res;
     int [][] cur;
@@ -20,17 +21,33 @@ public class NetworkFlow{
         while(find_res()){
             int min = Integer.MAX_VALUE; 
             for(int i = 0;i < n; i++){
+                if(i == s)
+                    continue;
                 int j = pre[i];
                 int value = res[i][j];
                 if(value < min)
                     min = value;
             }
             result += min;
-            for(int i = 0 ; i < n ; i++)
-            for(int j = 0 ; j < n ; j++){
-                cur
+            System.out.println(min);
+            for(int j = 0 ; j < n ; j ++){
+                int i = pre[j];
+                if(graph[i][j] >= cur[i][j] + min)
+                {
+                    cur[i][j] += min;
+                    res[i][j] -= min;
+                    res[j][i] += min;
+                }
+                else if(graph[i][j] < cur[i][j] + min)
+                {
+                    cur[j][i] = cur[j][i] - (min + cur[i][j] - graph[i][j]);
+                    cur[i][j] = graph[i][j];
+                    res[i][j] -= min;
+                    res[j][i] += min;
+                }
             }
         }
+        return result;
     }
 
     boolean find_res(){
@@ -55,5 +72,17 @@ public class NetworkFlow{
             }
         }
         return result;
+    }
+
+    public static void main(String args[]){
+        NetworkFlow nf = new NetworkFlow();
+        int g[][] = new int[4][4];
+        g[0][1] = 2;
+        g[0][2] = 3;
+        g[2][1] = 2;
+        g[2][3] = 1;
+        g[1][3] = 4;
+        System.out.println(nf.solve(g,0,3));
+        
     }
 }
